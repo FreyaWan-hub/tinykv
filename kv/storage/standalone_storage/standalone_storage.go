dd7edb03b5b1b65252470facc1d406ab4f710f81
FreyaWan <wan_qianhui@163.com>
2022-10-23 23:10:45 +0800

    第一次版本提交

--- kv/storage/standalone_storage/standalone_storage.go
+++ kv/storage/standalone_storage/standalone_storage.go
@@ -1,8 +1,11 @@
 package standalone_storage
 
 import (
+	"github.com/Connor1996/badger"
 	"github.com/pingcap-incubator/tinykv/kv/config"
 	"github.com/pingcap-incubator/tinykv/kv/storage"
+	"github.com/pingcap-incubator/tinykv/kv/util/engine_util"
+	"github.com/pingcap-incubator/tinykv/log"
 	"github.com/pingcap-incubator/tinykv/proto/pkg/kvrpcpb"
 )
 
@@ -10,15 +13,32 @@ import (
 // communicate with other nodes and all data is stored locally.
 type StandAloneStorage struct {
 	// Your Data Here (1).
+	DB badger.DB
+	StorageReader storage.StorageReader
+
 }
+
+type Reader struct{
+
+}
+
 
 func NewStandAloneStorage(conf *config.Config) *StandAloneStorage {
 	// Your Code Here (1).
+	opts := badger.DefaultOptions
+	opts.Dir = conf.DBPath
+	opts.ValueDir = conf.DBPath
+	db, err := badger.Open(opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	return nil
 }
 
 func (s *StandAloneStorage) Start() error {
 	// Your Code Here (1).
+
 	return nil
 }
 
@@ -35,4 +55,16 @@ func (s *StandAloneStorage) Reader(ctx *kvrpcpb.Context) (storage.StorageReader,
 func (s *StandAloneStorage) Write(ctx *kvrpcpb.Context, batch []storage.Modify) error {
 	// Your Code Here (1).
 	return nil
+}
+
+func (r *Reader) GetCF(cf string, key []byte) ([]byte, error){
+	return nil,nil
+}
+
+func (r *Reader) IterCF(cf string) engine_util.DBIterator{
+	return nil
+}
+
+func (r *Reader) Close(){
+
 }
